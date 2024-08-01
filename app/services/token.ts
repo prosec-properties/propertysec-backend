f0c198d86c27052206fb341780b293189cf1361f
ansman58 <javanslem@gmail.com>
2024-08-01 07:15:59 +0100

    rm pnpm lock from git cache

--- app/services/token.ts
+++ app/services/token.ts
@@ -4,13 +4,13 @@ import User from '#models/user'
 import redis from '@adonisjs/redis/services/main'
 import { v4 as uuidv4 } from 'uuid'
 
-export default class AuthToken {
+export default class AuthTokenService {
   static async generatePasswordResetToken(user: User | null) {
     const token = uuidv4()
 
     if (!user) return token
 
-    // Key is ignored if it is available
+    // Key is ignored if it is not available
     await redis.del(`${REDIS_RESET_PASSWORD_PREFIX}${user.id}`)
 
     await redis.setex(
@@ -27,7 +27,7 @@ export default class AuthToken {
 
     if (!user) return token
 
-    // Key is ignored if it is available
+    // Key is ignored if it is not available
     await redis.del(`${REDIS_VERIFY_EMAIL_PREFIX}${user.id}`)
 
     await redis.set(`${REDIS_VERIFY_EMAIL_PREFIX}${user.id}`, token)
