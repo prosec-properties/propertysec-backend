a3e1004c5c06552a85ffef3473e296db9d90f31e
ansman58 <javanslem@gmail.com>
2024-08-02 20:24:05 +0100

    update

--- config/database.ts
+++ config/database.ts
@@ -6,15 +6,23 @@ const dbConfig = defineConfig({
   connections: {
     postgres: {
       client: 'pg',
+      ...(env.get('DB_VERSION') ? { version: env.get('DB_VERSION') } : {}),
       connection: {
+        ...(env.get('DB_CONNECTION_STRING')
+          ? { connectionString: env.get('DB_CONNECTION_STRING') }
+          : {}),
         host: env.get('DB_HOST'),
         port: env.get('DB_PORT'),
         user: env.get('DB_USER'),
         password: env.get('DB_PASSWORD'),
         database: env.get('DB_DATABASE'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       migrations: {
         naturalSort: true,
+        disableRollbacksInProduction: true,
         paths: ['database/migrations'],
       },
     },
