6209549fe3dada181c083e19de987ebbbbfd6cdb
ansman58 <javanslem@gmail.com>
2024-08-12 04:15:19 +0100

    fix

--- app/middleware/logger_middleware.ts
+++ app/middleware/logger_middleware.ts
@@ -3,16 +3,12 @@ import type { HttpContext } from '@adonisjs/core/http'
 export default class LoggerMiddleware {
   async handle(ctx: HttpContext, next: () => Promise<void>) {
     const { request, logger } = ctx
-    let log = {} as any
 
     logger.info(
       {
         method: request.method(),
         url: request.url(true),
         logTime: new Date().toISOString(),
-        // ips: request.ips(),
-        // traceId: request.id(),
-        // ...log,
       },
       'Incoming Request'
     )
