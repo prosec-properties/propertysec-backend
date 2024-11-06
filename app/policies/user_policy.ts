2e37e58c0ff4cc9d24539f764ca3512bd6db9ff1
ansman58 <javanslem@gmail.com>
2024-11-06 23:08:43 +0100

    rm unused var

--- app/policies/user_policy.ts
+++ app/policies/user_policy.ts
@@ -1,6 +1,5 @@
 import User from '#models/user'
 import { BasePolicy } from '@adonisjs/bouncer'
-import { AuthorizerResponse } from '@adonisjs/bouncer/types'
 
 export default class UserPolicy extends BasePolicy {
   isAdmin(user: User) {
